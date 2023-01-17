c6c77d7bf2852d1edf170b300de2de5b6e3f6471
shreealagh <shreealag60@gmail.com>
2023-01-17 16:49:50 +0530

    requireandexports

---
+++ RequireandModuleexports/index.js
@@ -0,0 +1,6 @@
+const {PI,square,add}=require('./math');
+
+
+console.log(PI);
+console.log(square(2));
+console.log(add(5,3));
---
+++ RequireandModuleexports/math.js
@@ -0,0 +1,11 @@
+const PI=3.14;
+const square=(num)=>num**2;
+
+const add=(num1,num2)=>num1+num2;
+
+
+module.exports={
+    PI,
+    square,
+    add
+}
